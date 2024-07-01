7e173179f13ee9e7d6b8edd32425fda4da7c00cc
Kobayashi0822 <49467240+Kobayashi0822@users.noreply.github.com>
2024-07-01 18:31:10 +0900

    [UPD] install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,11 @@
 
 echo "╔══╣ Install: HSR Description (STARTING) ╠══╗"
 
-cd ~/catkin_ws/src/
-
 git clone https://github.com/TeamSOBITS/hsr_description.git
 
 echo "╚══╣ Install: HSR Description (FINISHED) ╠══╝"
 
 echo "╔══╣ Install: HSR Meshes (STARTING) ╠══╗"
-
-cd ~/catkin_ws/src/
 
 git clone https://github.com/TeamSOBITS/hsr_meshes.git
 
