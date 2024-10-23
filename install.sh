6fd0040adec4fda5c2fca07a9b76c7340ef86885
Kobayashi0822 <49467240+Kobayashi0822@users.noreply.github.com>
2024-10-23 16:32:19 +0900

    [UPD] install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,45 @@
 #! /bin/bash
 
-roscd
+echo "╔══╣ Install: HSR SIM COMMON (STARTING) ╠══╗"
 
-echo "╔══╣ Install: HSR Description (STARTING) ╠══╗"
-cd ../
+# Keep track of the current directory
+DIR=`pwd`
 
+# Install mongo_c
+mkdir ~/mongo_c
+cd ~/mongo_c/
+wget https://github.com/mongodb/mongo-c-driver/releases/download/1.4.2/mongo-c-driver-1.4.2.tar.gz
+tar zxvf mongo-c-driver-1.4.2.tar.gz
+cd mongo-c-driver-1.4.2/
+./configure
+make
+sudo make install
+
+# Install mongo_cpp
+mkdir ~/mongo_cpp
+cd ~/mongo_cpp/
+wget https://github.com/mongodb/mongo-cxx-driver/archive/r3.0.3.tar.gz
+tar zxvf r3.0.3.tar.gz
+cd mongo-cxx-driver-r3.0.3/build/
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DLIBMONGOC_DIR=/usr/local -DLIBBSON_DIR=/usr/local ..
+sudo make EP_mnmlstc_core
+make
+sudo make install
+
+# Install sigverse_ros_package
+cd $DIR/..
+git clone  https://github.com/SIGVerse/sigverse_ros_package.git
 git clone https://github.com/TeamSOBITS/hsr_description.git
-
-echo "╚══╣ Install: HSR Description (FINISHED) ╠══╝"
-
-echo "╔══╣ Install: HSR Meshes (STARTING) ╠══╗"
-
 git clone https://github.com/TeamSOBITS/hsr_meshes.git
 
-echo "╚══╣ Install: HSR Meshes (FINISHED) ╠══╝"
+# Install ROS dependecies
+sudo apt-get update
+sudo apt-get install -y \
+    ros-${ROS_DISTRO}-roswww \
+    ros-${ROS_DISTRO}-rosbridge-suite \
+    ros-${ROS_DISTRO}-jsk-rviz-plugins \
+    ros-${ROS_DISTRO}-rosbridge-server
 
-echo "Install ros-${ROS_DISTRO}-roswww"
-sudo apt-get install ros-${ROS_DISTRO}-roswww -y
+cd $DIR
 
-echo "Install ros-${ROS_DISTRO}-rosbridge-suite"
-sudo apt-get install ros-${ROS_DISTRO}-rosbridge-suite -y
-
-echo "Install ros-${ROS_DISTRO}-jsk-rviz-plugins"
-sudo apt-get install ros-${ROS_DISTRO}-jsk-rviz-plugins -y
-
-echo "Install Finished"
+echo "╚══╣ Install: HSR SIM COMMON (FINISHED) ╠══╝"
