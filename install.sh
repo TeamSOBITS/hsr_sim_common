2a408f50571e23a2e5e5e58027ea857b43cdddfb
f22hakuti <131255788+f22hakuti@users.noreply.github.com>
2025-04-04 18:49:39 +0900

    add to install depth-image-proc

--- install.sh
+++ install.sh
@@ -41,6 +41,7 @@ sudo apt-get update
 sudo apt-get install -y \
     ros-${ROS_DISTRO}-jsk-rviz-plugins \
     ros-${ROS_DISTRO}-nav2-msgs \
+    ros-${ROS_DISTRO}-depth-image-proc \
     ros-${ROS_DISTRO}-rosbridge_library  #####
 
 pip3 install --upgrade pip
