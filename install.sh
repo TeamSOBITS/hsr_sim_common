df84a961d527df82396f9a1fe2301040c482ca8c
apparettedare <ksuzuki9541@gmail.com>
2025-04-11 15:26:58 +0900

    Add minimal launch

--- install.sh
+++ install.sh
@@ -36,18 +36,18 @@ sudo make install
 cd $DIR/..
 git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sigverse_ros_package.git
 git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/rosbridge_suite.git
+git clone -b ${ROS_DISTRO} https://github.com/hsr-project/hsrb_description.git
+git clone -b ${ROS_DISTRO} https://github.com/hsr-project/hsrb_meshes.git
+
 # Install ROS dependecies
 sudo apt-get update
 sudo apt-get install -y \
     ros-${ROS_DISTRO}-jsk-rviz-plugins \
     ros-${ROS_DISTRO}-nav2-msgs \
     ros-${ROS_DISTRO}-depth-image-proc \
-    ros-${ROS_DISTRO}-rosbridge_library  #####
+    ros-${ROS_DISTRO}-rosbridge_library 
 
 pip3 install --upgrade pip
 pip3 install tornado
-
-cd ~/colcon_ws/
-colcon build --symlink-install
 
 echo "╚══╣ Install: HSR SIM COMMON (FINISHED) ╠══╝"
---
+++ launch/minimal.launch.py
@@ -0,0 +1,52 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+import xacro
+
+
+def generate_launch_description():
+    description_pkg = "hsrb_description"
+    rviz_config = os.path.join(get_package_share_directory(
+        "hsr_sim_common"), "rviz", "hsr.rviz")
+    robot_description = os.path.join(get_package_share_directory(
+        description_pkg), "robots", "hsrb4s.urdf.xacro")
+    robot_description_config = xacro.process_file(robot_description)
+
+    generate_pointcloud = os.path.join(
+        get_package_share_directory("hsr_sim_common"), "launch", "generate_pointcloud.launch.py"
+    )
+    library_server = os.path.join(
+        get_package_share_directory("hsr_sim_common"), "launch", "library_server.launch.py"
+    )
+    return LaunchDescription([
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[
+                {"robot_description": robot_description_config.toxml()}],
+            output="screen",
+        ),
+
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rviz_config],
+            output="screen",
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(generate_pointcloud)
+        ),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(library_server)
+        ),
+    ])
