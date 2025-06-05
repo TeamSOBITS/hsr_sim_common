7ced6952029281a14d7bf93e24e57c213fae9c86
OnoFumiya <fumiyaono.choi@gmail.com>
2025-06-05 21:42:14 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -42,7 +42,6 @@ git clone -b ${ROS_DISTRO} https://github.com/hsr-project/hsrb_meshes.git
 # Install ROS dependecies
 sudo apt-get update
 sudo apt-get install -y \
-    ros-${ROS_DISTRO}-jsk-rviz-plugins \
     ros-${ROS_DISTRO}-nav2-msgs \
     ros-${ROS_DISTRO}-depth-image-proc \
     ros-${ROS_DISTRO}-rosbridge-library 
