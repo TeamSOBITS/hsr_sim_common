fc1d642efd8a3b48897300ccb365583a18f8d542
Lucasmotabr <e26m5348@soka-u.jp>
2026-07-11 18:50:20 +0900

    fix: build mongo-cxx-driver r3.11.0 for Jazzy / Ubuntu 24.04
    Old mongo-c 1.4.2 + mongo-cxx r3.0.3 (2016) no longer compile on gcc-13.
    r3.11.0 bundles a compatible C driver (mongo-c 1.28.0) via submodule, so
    the separate mongo-c build is dropped.

--- install.sh
+++ install.sh
@@ -11,26 +11,23 @@ cd sobits_interfaces
 bash install.sh
 cd $DIR
 
-# Install mongo_c
-mkdir -p ~/mongo_c
-cd ~/mongo_c/
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.4.2/mongo-c-driver-1.4.2.tar.gz
-tar zxvf mongo-c-driver-1.4.2.tar.gz
-cd mongo-c-driver-1.4.2/
-./configure
-make
-sudo make install
-
-# Install mongo_cpp
+# Install MongoDB C++ driver (mongo-cxx-driver r3.11.0)
+# NOTE (Jazzy / Ubuntu 24.04): the old mongo-c 1.4.2 + mongo-cxx r3.0.3 source builds
+# no longer compile on gcc-13. r3.11.0 bundles a compatible C driver (auto-downloads
+# mongo-c 1.28.0 via submodule), so a separate mongo-c build is no longer needed.
+rm -rf ~/mongo_cpp
 mkdir -p ~/mongo_cpp
 cd ~/mongo_cpp/
-wget https://github.com/mongodb/mongo-cxx-driver/archive/r3.0.3.tar.gz
-tar zxvf r3.0.3.tar.gz
-cd mongo-cxx-driver-r3.0.3/build/
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DLIBMONGOC_DIR=/usr/local -DLIBBSON_DIR=/usr/local ..
-sudo make EP_mnmlstc_core
-make
+git clone https://github.com/mongodb/mongo-cxx-driver.git
+cd mongo-cxx-driver
+git checkout r3.11.0
+git submodule update --init --recursive
+rm -rf build && mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_CXX_STANDARD=17
+make -j$(nproc)
 sudo make install
+sudo ldconfig
+cd $DIR
 
 # Install sigverse_ros_package
 cd $DIR/..
